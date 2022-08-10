6807d76a99a547822eef4e16e9947e7b85e2bb43
SarvarVakhobov <sarvarbek0125@gmail.com>
2022-08-10 08:45:27 +0500

    Custom user

--- config/settings.py
+++ config/settings.py
@@ -37,6 +37,14 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    #3rd part apps
+    'rest_framework',
+    'rest_framework.authtoken',
+
+    #my apps
+    'timetable',
+    'user',
 ]
 
 MIDDLEWARE = [
@@ -123,3 +131,6 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+AUTH_USER_MODEL = 'user.CustomUser'
+
